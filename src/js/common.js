83a72663cd6f8a8d3024f21a3a46f9c70ab421b7
c011322371 <c011322371@edu.teu.ac.jp>
2018-03-28 18:37:19 +0900

    ギャラリー作成

---
+++ src/js/common.js
@@ -0,0 +1,87 @@
+var CREATE_TOP_THUMBNAIL = {
+  load: function () {
+    $.ajax({
+      url: "/src/json/topthumb.json",
+      type: 'GET',
+      dataType: 'json',
+      cache: false
+    }).then(
+      $.proxy(this.init, this)
+    ).catch(function () {
+      console.log('error');
+    })
+  },
+  init: function (data) {
+    this.jsonData = data;
+    this.setParameters();
+    this.bindEvent();
+  },
+  setParameters: function () {
+    this.thumbList = $('.jsc-nav-list');
+    this.thumbItems = this.thumbList.children('li');
+  },
+  bindEvent: function () {
+    // topのnavのcassettesを作成
+    this.createCassettes();
+
+    // 画像の読み込みが終わり次第アニメーション開始
+    $(window).on('load', $.proxy(function () {
+      this.kvAnimation();
+    }, this));
+  },
+  createCassettes: function () {
+    var fragment = document.createDocumentFragment();
+    this.$template = this.thumbItems.detach();
+
+    for (var i = 0; i < this.jsonData.length; i++) {
+      this.$cassette = this.$template.clone();
+      this.$cassette.css({ 'background-image': "url(" + this.jsonData[i].img + ")" });
+      this.$cassette.find('a').attr("href", this.jsonData[i].href);
+      if (this.$cassette.find('p').length) this.$cassette.find('p').html(this.jsonData[i].description);
+      fragment.appendChild(this.$cassette.get(0));
+    }
+    this.thumbList.get(0).appendChild(fragment);
+  },
+  kvAnimation: function () {
+    // casettesを生成したため、変数ではなく再取得するような記述をしている
+    $('.jsc-top-nav-list li').each(function (index) {
+      $(this).delay(150 * index).fadeTo(1000, 1);
+    });
+  }
+}
+
+var MODAL_MANAGER = {
+  init: function () {
+    this.setParameters();
+    this.bindEvent();
+  },
+  setParameters: function () {
+    this.$body = $('body');
+    this.$modalTrigger = $('.jsc-modal-trigger');
+    this.$modalContents = $('.jsc-modal-contents');
+  },
+  bindEvent: function () {
+    var myself = this;
+
+    this.$modalTrigger.on('click', $.proxy(function (e) {
+      this.showModal();
+    }, this));
+
+    this.$modalContents.on('click', $.proxy(function () {
+      this.hideModal();
+    }, this));
+  },
+  showModal: function () {
+    this.$modalContents.fadeIn();
+    this.$body.css({ overflow: "hidden" });
+  },
+  hideModal: function () {
+    this.$modalContents.fadeOut();
+    this.$body.css({ overflow: "auto" });
+  }
+};
+
+$(function () {
+  CREATE_TOP_THUMBNAIL.load();
+  MODAL_MANAGER.init();
+});
--- src/js/subPage.js
+++ src/js/subPage.js
@@ -1,16 +1,86 @@
-var OBJECT = {
-  init: function() {
+var CREATE_THUMB = {
+  load: function () {
+    $.ajax({
+      url: "/src/json/subthumb.json",
+      type: 'GET',
+      dataType: 'json',
+      cache: false
+    }).then($.proxy(function (json) {
+      var pageName = $("body").attr("id");
+      for (var i = 0; i < json.length; i++) {
+        if (json[i].page === pageName) {
+          this.init(json[i]);
+        }
+      }
+    }, this)).catch(function () {
+      console.log('error');
+    })
+  },
+  init: function (json) {
+    this.imgPath = json.img;
     this.setParameters();
     this.bindEvent();
   },
-  setParameters: function() {
-    console.log('a');
+  setParameters: function () {
+    this.$previewContainer = $('.jsc-gallery-preview');
+    this.$thumbContainer = $('.jsc-gallery-thumb');
   },
-  bindEvent: function() {
+  bindEvent: function () {
+    this.createThumb();
+    this.createPreview();
+    this.$thumbList.on('click', $.proxy(this.applyPreview, this));
+  },
+  createThumb: function () {
+    var thumbCassettes = this.createCassettes();
+    this.$thumbContainer.get(0).appendChild(thumbCassettes);
+    // set created thumblist
+    this.$thumbList = this.$thumbContainer.children('li');
+  },
+  createPreview: function () {
+    var previewCassettes = this.createCassettes();
+    this.$previewContainer.get(0).appendChild(previewCassettes);
+    // set created previewlist
+    this.$previewList = this.$previewContainer.children('li');
+  },
+  createCassettes: function () {
+    var $li = $("<li />"),
+      fragment = document.createDocumentFragment();
 
+    for (var i = 0; i < this.imgPath.length; i++) {
+      var $cassette = $li.clone();
+      $cassette.css({ 'background-image': "url(" + this.imgPath[i] + ")" });
+
+      fragment.appendChild($cassette.get(0));
+    }
+    return fragment;
+  },
+  applyPreview: function (e) {
+    var index = $(e.target).index(),
+      ANIMATION_SPEED = 800;
+    this.$previewList.fadeOut(ANIMATION_SPEED);
+    this.$previewList.eq(index).fadeIn(ANIMATION_SPEED);
+    return false;
   }
 };
 
-$(function() {
-  OBJECT.init();
+var TOGGLE_NAV_MENU = {
+  init: function () {
+    this.setParameters();
+    this.bindEvent();
+  },
+  setParameters: function () {
+    this.$navIcon = $('.jsc-nav-icon');
+    this.$navList = $('.jsc-nav-wrap');
+  },
+  bindEvent: function () {
+    this.$navIcon.on('click', $.proxy(this.toggleNavMenu, this));
+  },
+  toggleNavMenu: function () {
+    this.$navList.fadeToggle()
+  }
+};
+
+$(function () {
+  CREATE_THUMB.load();
+  TOGGLE_NAV_MENU.init();
 });
